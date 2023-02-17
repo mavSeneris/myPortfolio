e05dcf7575ba6e229def1ea68db410cf5de58845
Maverick Seneris <mavseneris@gmail.com>
2023-02-17 15:53:58 +0800

    added new project: etape ecommerce ui

--- src/projects.js
+++ src/projects.js
@@ -9,4 +9,15 @@ export default [
         stacks: ["HTML", "CSS", "JavaScript", "React"],
         type: "game"
     },
+
+    {
+        id: 2,
+        name: "E-Commerce UI",
+        image: "../src/assets/images/hero-image1.2a8a1f04.png",
+        url: "https://polite-brioche-7bea1b.netlify.app/",
+        gitHub: "https://github.com/mavSeneris/etape",
+        description: "You have ten dice and you must roll as fast as you can until all your dice is on the same number in the shortest time and shout TENZI!",
+        stacks: ["HTML", "CSS", "JavaScript", "React"],
+        type: "website"
+    }
 ]
